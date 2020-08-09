d83d0877ac2ccd8e34fd3f8a1042b5950b83e10b
Yogesh Khichi <yogeshkkhichi@gmail.com>
2020-08-09 19:05:36 +0530

    2nd9aug

---
+++ ThePropelQuesion/Notes.js
@@ -0,0 +1,28 @@
+// console.log(Array.from(new Set('cabcd'.split(''))));
+// // if objProp in obj
+
+// console.log(Math.floor(3/2));
+
+
+
+// console.log(([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]).map((a) => a[0]-a[1]).reduce(Func,0));
+// function Func(acc,curr)
+// {
+//     return acc+curr;
+// }
+
+
+// remeber the typeof(function  )
+
+// let str = 'sip is sipping'
+
+// console.log(str.indexOf('sip',str.indexOf('sip')+1))
+
+// let obj = {hello: "greeting"};
+
+// let obj2 = Object.assign({},obj);
+// obj2.hello = 'hi';
+// console.log(obj2);
+// let arr = [1,2,3];
+// console.log(JSON.parse(JSON.stringify(obj)));
+// console.log(JSON.parse(JSON.stringify(arr)));
